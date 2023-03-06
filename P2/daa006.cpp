1b445a119de713472844ebdc5b04f15c304c7a34
Marisa Azevedo <marisa.azevedo@Marisas-MacBook-Pro.local>
2023-03-06 15:42:54 +0000

    daa006

--- P2/daa006.cpp
+++ P2/daa006.cpp
@@ -2,37 +2,39 @@
 #define M_PI 3.1415926535897
 
 using namespace std;
-double resolucao (double qx, double qy, double ql, double cx, double cy, double cr) {
-        // qx  ----------->  x esquerda do quadrado
-        // qy  ----------->  y inferior do quadrado
-    double qx2 = qx + ql; // x direito do quadrado
-	double qy2 = qy + ql; // y superior do quadrado
-    double cx1 = cx - cr; // posicao à esquerda do circulo
-	double cy1 = cy - cr; // posicao inferior do circulo
-    double cx2 = cx + cr; // posicao à direita do circulo
-	double cy2 = cy + cr; // posicao superior
+
+double resolucao (double qx1, double qy1, double ql, double cx, double cy, double cr) {
+        // qx1  ----------->  x esquerda do quadrado
+        // qy2  ----------->  y inferior do quadrado
+    double qx2 = qx1 + ql; // x direito do quadrado
+	double qy2 = qy1 + ql; // y superior do quadrado
+    double cx1 = cx - cr;  // posicao à esquerda do circulo
+	double cy1 = cy - cr;  // posicao inferior do circulo
+    double cx2 = cx + cr;  // posicao à direita do circulo
+	double cy2 = cy + cr;  // posicao superior
     double intersecao = 0;
 
-    if(cx1 > qx && cx2 < qx2 && cy1 > qy && cy2 < qy2) {
-        return M_PI * cr * cr;
-    } // circulo dentro do quadrado
-
-    else if((qx - cx) * (qx - cx) + (qy - cy) * (qy - cy) < cr * cr && (qx - cx) * (qx - cx) + (qy2 - cy) * (qy2 - cy) < cr * cr && (qx2 - cx) * (qx2 - cx) + (qy - cy) * (qy - cy) < cr * cr && (qx2 - cx) * (qx2 - cx) + (qy2 - cy) * (qy2 - cy) < cr * cr) {
-        return ql * ql;
-    } // quadrado dentro do circulo
-
-    else if(cx2 < qx || cx1 > qx2 || cy2 < qy || cy1 > qy2) {
+    if(((qx1 - cx) * (qx1 - cx) + (qy1 - cy) * (qy1 - cy) > cr * cr) && ((qx1 - cx) * (qx1 - cx) + (qy2 - cy) * (qy2 - cy) > cr * cr) && ((qx2 - cx) * (qx2 - cx) + (qy1 - cy) * (qy1 - cy) > cr * cr) && ((qx2 - cx) * (qx2 - cx) + (qy2 - cy) * (qy2 - cy) > cr * cr) && (!(cx > qx1 && cx < qx2)) && (!(cy > qy1 && cy < qy2))) {
+        // uma figura está garantidamente fora da outra
         return 0;
-    } // uma figura está garantidamente fora da outra
+    }
 
+    else if((cx1 > qx1) && (cx2 < qx2) && (cy1 > qy1) && (cy2 < qy2)) {
+        // circulo dentro do quadrado
+        return M_PI * cr * cr;
+    }
 
+    else if((((qx1 - cx) * (qx1 - cx) + (qy1 - cy) * (qy1 - cy)) < (cr * cr)) && (((qx1 - cx) * (qx1 - cx) + (qy2 - cy) * (qy2 - cy)) < (cr * cr)) && (((qx2 - cx) * (qx2 - cx) + (qy1 - cy) * (qy1 - cy)) < (cr * cr)) && (((qx2 - cx) * (qx2 - cx) + (qy2 - cy) * (qy2 - cy)) < (cr * cr))) {
+        // quadrado dentro do circulo
+        return ql * ql;
+    }
 
-    else if(ql >= 0.001) { // MAL -> tentar perceber depois !!!
+    else if(ql >= 0.001) {
         ql /= 2;
-        intersecao += resolucao(qx,  qy,  ql, cx, cy, cr);
-		intersecao += resolucao(qx2, qy,  ql, cx, cy, cr);
-		intersecao += resolucao(qx,  qy2, ql, cx, cy, cr);
-		intersecao += resolucao(qx2, qy2, ql, cx, cy, cr);
+        intersecao += resolucao(qx1,      qy1 + ql, ql, cx, cy, cr);
+		intersecao += resolucao(qx1 + ql, qy1 + ql, ql, cx, cy, cr);
+		intersecao += resolucao(qx1,      qy1,      ql, cx, cy, cr);
+		intersecao += resolucao(qx1 + ql, qy1,      ql, cx, cy, cr);
 
     }
     return intersecao;
