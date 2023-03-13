72dcc8e00954dbdfe8384f17ac9c3018c6ce8e20
Marisa Azevedo <marisa.azevedo@Marisas-MacBook-Pro.local>
2023-03-13 15:22:44 +0000

    implementation: count first indice that a char appear

--- P3/daa009.cpp
+++ P3/daa009.cpp
@@ -7,12 +7,15 @@ using namespace std;
 typedef struct {
     char letra;
     int freq;
+    int first_ind;
 } DNA;
 
 struct compareDNA {
     bool operator()(const DNA & p1, const DNA & p2) {
-        if (p1.freq < p2.freq) return true;
-        if (p1.freq > p2.freq) return false;
+        if (p1.freq < p2.freq) return false;
+        if (p1.freq > p2.freq) return true;
+        if (p1.freq == p2.freq && p1.first_ind < p2.first_ind) return true;
+        if (p1.freq == p2.freq && p1.first_ind > p2.first_ind) return false;
         return 0;
     }
 };
@@ -24,6 +27,7 @@ int main() {
     for(int i = 0 ; i < 26 ; i++) {
 		lista[i].letra = 'A' + i ;
 		lista[i].freq = 0;
+		lista[i].first_ind = -1;
 	}
 
     cin >> dna;
@@ -33,9 +37,15 @@ int main() {
         lista[dna[i] - 'A'].freq++;
     }
 
-    sort(lista, lista + size, compareDNA());
-
     for(int i = 0; i < size; i++) {
+        if(lista[dna[i] - 'A'].first_ind == -1) {
+            lista[dna[i] - 'A'].first_ind = i;
+        }
+    }
+
+    sort(lista, lista + 26, compareDNA());
+
+    for(int i = 0; i < 26; i++) {
         if(lista[i].freq != 0) {
             cout << lista[i].letra << " " << lista[i].freq << endl;
         }
